8a559d093bb45bf4bc958514da845d391c6a4ef6
Kamran Musayev <kamranmusayev90@gmail.com>
2020-04-24 23:05:54 +0400

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyC_7goHw8LWOzcVf0ickwRJmqpaX-jLu5o',
+  fbDbUrl: 'https://angular-tutorial-f3bd5.firebaseio.com'
+};
